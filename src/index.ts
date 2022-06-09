09271e3199bac2478c05f0604c9cda31b970d306
972389149 <972389149@qq.com>
2022-06-09 20:48:43 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export default 555
+class Animal {
+  name: string
+  constructor(name) {
+    this.name = name
+  }
+  sayName() {
+    console.log(this.name)
+    return this.name
+  }
+}
+
+
+export default Animal
